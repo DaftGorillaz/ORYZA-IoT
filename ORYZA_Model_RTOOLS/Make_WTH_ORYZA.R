fa7efd4ec4499e8d6a978632e88cf111a0d8add7
Lance Salmingo <salmingolance@gmail.com>
2025-05-24 14:55:39 +1000

    ORYZA_RTOOLS by jrodriguez88

---
+++ ORYZA_Model_RTOOLS/Make_WTH_ORYZA.R
@@ -0,0 +1,110 @@
+######################################################################################
+#######      Make ORYZA weather files by year                      #############
+####################    by JRE- https://github.com/jrodriguez88    ###################     
+######################################################################################
+
+
+### ORYZA weather file, include:
+
+#  Column    Daily Value                                                                         
+#     1      Station number                                                                       
+#     2      Year                                                                           
+#     3      Day                                                                    
+#     4      irradiance         KJ m-2 d-1                                            
+#     5      min temperature            oC                                                        
+#     6      max temperature            oC                                                        
+#     7      vapor pressure            kPa                                                        
+#     8      mean wind speed         m s-1                                                        
+#     9      precipitation          mm d-1
+
+
+## Make_WTH_ORYZA function compute weather information to ORYZA weather file.
+## 'data':  csv file name or data.frame. 
+#           str-> 
+#                ..$ DATE: Date ->(mdy)
+#                ..$ TMAX: num  ->(oC)
+#                ..$ TMIN: num  ->(oC) 
+#                ..$ RAIN: num  ->(mm) 
+#                ..$ SRAD: num  ->(MJ) 
+#                ..$ RHUM: num  ->(%) 
+## 'path':  path folder or working directory
+## 'local': 4 letters string of locality name. "AIHU"--> Aipe, Huila
+## 'lat':   latitud (decimal degrees)
+## 'lon':   longitud (decimal degrees)
+## 'alt':   altitude (meters above sea level)
+
+
+make_wth_oryza <- function(data, path, local, lat, lon, alt, stn=1) {
+    stopifnot(require(tidyverse)==T)
+    stopifnot(require(lubridate)==T)
+    if(any(class(data)=="data.frame")){
+        DATA <- data %>%
+            mutate(DATE = as.Date(DATE, format= "%m/%d/%Y"),
+                   Station_number = stn,
+                   Year = year(DATE),
+                   Day = yday(DATE),
+                   SRAD = round(SRAD*1000, 2),
+                   TMAX = round(TMAX, 2),
+                   TMIN = round(TMIN, 2),
+                   RAIN = round(RAIN, 2),
+                   VPD = -99, 
+                   WS = -99) %>%
+            select(Station_number, Year, Day, SRAD, TMIN, TMAX, VPD, WS, RAIN)
+    
+        } else if(str_detect(data, ".csv")==T){
+    DATA <- read.csv(paste0(path,"//", data), header = T) %>%
+        mutate(DATE = as.Date(DATE, format= "%m/%d/%Y"),
+               Station_number = stn,
+               Year = year(DATE),
+               Day = yday(DATE),
+               SRAD = round(SRAD*1000, 2),
+               TMAX = round(TMAX, 2),
+               TMIN = round(TMIN, 2),
+               RAIN = round(RAIN, 2),
+               VPD = -99, 
+               WS = -99) %>%
+        select(Station_number, Year, Day, SRAD, TMIN, TMAX, VPD, WS, RAIN)
+        
+        } else {message("data no recognized")}
+    
+dir.create(paste0(path,"/WTH"), showWarnings = FALSE)
+set_head <- paste(lon, lat, alt, 0, 0, sep = ",")    
+#DATA=read.table(file, head=T)  
+data_list <- split(DATA, DATA$Year)
+lapply(data_list, function(x){
+    fname <- paste(path,"/WTH/" ,local, stn,".", str_sub(unique(x$Year), 2), sep = "")
+    sink(file=fname)
+    cat(set_head)
+    cat("\n")
+    write.table(x ,sep=",",row.names=F,col.names=F)
+    sink()})
+
+}
+
+#map(data_list, copy_wth)
+#data <- "weather_input.csv"
+#path <- getwd() 
+#local<- "AIHU"
+#lat <- 3.5
+#lon <- -75.5
+#alt <- 250
+#Make_WTH_ORYZA(AIHU$WTH_obs, path,local, 3.4, -72, 250)
+#Make_WTH_ORYZA("weather_input.csv", path,local, 3.4, -72, 250)
+
+#library(tidyverse)
+#list.files(pattern=".RData") %>% lapply(load, .GlobalEnv)
+#locs_id <- ls()
+
+#path <- getwd()
+wdata_list <- function (data, path) {
+    wdata <- data$WTH_obs
+    local <- data$AGRO_man$LOC_ID[1]
+    lat <- data$AGRO_man$LAT[1]
+    lon <- data$AGRO_man$LONG[1]
+    alt <- data$AGRO_man$ALT[1]
+    
+    make_wth_oryza(wdata, path, local, lat, lon, alt)
+    }
+
+
+#lapply(locs_id, wdata)
---
+++ ORYZA_Model_RTOOLS/README.md
@@ -0,0 +1,95 @@
+
+# ORYZA_Model_RTOOLS: Tools for Rice Crop Simulation with ORYZA V3.0
+### *Rodriguez-Espinoza J.*
+#### International Center for Tropical Agriculture
+#### Contact: [Email](mailto:j.r.espinosa@cgiar.org) - [LinkedIn](https://www.linkedin.com/in/jeferson-rodriguez-espinoza-24749625/)
+#### Repository: [GitHub](https://github.com/jrodriguez88/ORYZA_Model_RTOOLS)
+
+## Description
+Project with many tools to use ORYZA crop model. It Includes functions to create input files (weather `*.WTH`, soil `*.SOL`, experimental `*.EXP`), estimate parameters (crop, weather and soil), import and visualize outputs. It will conect with [ORYZA_AUTO_PARAM](https://github.com/jrodriguez88/ORYZA_AUTO_PARAM) tool to compute diferent parameters required in the crop model parametrization and calibration.
+
+#### For more information about the ORYZA Crop Model:
+
+* Official [ORYZA Model website](https://sites.google.com/a/irri.org/oryza2000/about-oryza-version-3)
+
+* Book about [ORYZA2000: modeling lowland rice.](http://irri.org/resources/publications/books/item/oryza2000-modeling-lowland-rice) 
+
+* Research paper about [ORYZA V3.0](https://www.sciencedirect.com/science/article/pii/S0168192317300680)
+
+
+## Make_WTH_ORYZA.R
+Function to create ORYZA-model weather files.
+
+### Usage
+```
+require(tidyverse)
+require(lubridate)
+Make_WTH_ORYZA(data, path, local, lat, lon, alt, stn=1)
+```
+
+### Arguments
+
+* `data:` csv file name or data.frame of daily values. [View Weather Template](https://raw.githubusercontent.com/jrodriguez88/ORYZA_Model_RTOOLS/master/weather_input.csv)
+
+| Var_Name | Description  |  Class| Unit |
+| --- | --- | --- | --- | 
+| DATE |  Date in "mm/dd/yyyy" | Date | MM/DD/YYYY |
+| TMAX |  Maximum temperature | num  | (oC) |
+| TMIN |  Minimun temperature | num  | (oC) |
+| RAIN |  Rain or Precipitation | num  | (mm) |
+| SRAD |  Solar Radiation | num  | (MJ) |
+| RHUM |  Relative humidity (opcional) | num  | (%)  |
+
+* `path:`      path folder or working directory
+* `local:`     4 letters string of locality name. "AIHU"--> Aipe, Huila
+* `lat:`       latitud (decimal degrees)
+* `lon:`       longitud (decimal degrees)
+* `alt:`       altitude (meters above sea level)
+* `stn:`       Station number. default=1
+
+## Make_SOIL_ORYZA.R
+Function to create ORYZA-model soil files. Default PADDY model
+
+### Usage
+```
+require(tidyverse)
+Make_SOIL_ORYZA(data, path, ZRTMS = 0.50, WL0I = 0, WCLI='FC' , RIWCLI = 'NO', SATAV=20)
+```
+
+### Arguments
+
+* `data:` csv file name or data.frame. [View Soil Template](https://raw.githubusercontent.com/jrodriguez88/ORYZA_Model_RTOOLS/master/soil_input.csv)
+
+| Var_Name  | Description | Class| Unit |
+| --- | --- | --- | --- |
+| ID |	Trial ID |	character | XXXX |
+| SAMPLING_DATE | Sampling date		| date	| 	MM/DD/YYYY	| 
+| DEPTH |	Soil depth (Thickness) | 	numeric	| 	cm	| 
+| PH |	pH in water	| 	numeric	| 	units	| 
+| SCARBON |	Total soil carbon	| 	numeric	| 	g/kg	| 
+| SCEC| Cation exchange capacity	| 	numeric	| 	cmol/kg	| 
+| SNH4| Ammonium, KCl, g elemental N	| 	numeric	| 	mg/kg	| 
+| SNO3| Nitrate, KCl, g elemental N	|  	numeric	| 	mg/kg	| 
+| SLNI| Total nitrogen	| 	numeric	| 	mg/kg	| 
+| SLON| Organic nitrogen	| 	numeric	| 	mg/kg	| 
+| SBDM | Bulk density, moist 	| 	numeric	| 	g/cm³	| 
+| SRD| Real density	| 	numeric	| 	g/cm³	| 	| 
+| SPOR| Total soil porosity	| 	numeric	| 	%	| 
+| WCF| Water content at field	| 	numeric	| 	%	| 
+| SSKS| Saturated hydraulic conductivity	| 	numeric	| 	cm/day	| 
+| WCST| Saturated volumetric water content	| 	numeric	| 	%	| 
+| WCFC| Volumetric water content at field capacity	| 	numeric	| 	%	| 
+| WC1B| Volumetric water content at 1 bar	| 	numeric	| 	%	| 
+| WCWP| Volumetric water content at wilting point	| 	numeric	| 	%	| 
+| SAND	| Soil sand content	| numeric	| 	%	| 
+| SILT	| Soil silt content	| numeric	| 	%	| 
+| CLAY 	| Soil clay content	| numeric	| 	%	| 
+| STC	| Soil texture class (USDA)	| 	character	| 	name	| 
+
+* `path:`   path folder or working directory
+* `ZRTMS:`  Maximum rooting depth in the soil (m), 
+* `WL0I:`   Initial ponded water depth at start of simulation (mm)"
+* `WCLI:`   can take 3 values: Field Capacity ('FC'), 50% of Soil Saturation ('ST50'), Fraction of water content ('0.0'- '1.0') 
+* `RIWCLI:` Re-initialize switch RIWCLI is YES or NO
+* `SATAV:`  Soil annual average temperature of the first layers
+
